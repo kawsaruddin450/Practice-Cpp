a293e9050cdce4f7daca1cf871db764a6ecacfc9
kawsar uddin patoary <kawsaruddin450@gmail.com>
2024-10-31 18:44:39 +0600

    constant in c++

---
+++ constant.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    const float PI = 3.14159;
+    float radius = 10;
+    float circumference = 2*PI*radius;
+
+    cout << circumference << " cm";
+    return 0;
+}
