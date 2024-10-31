ac2d5c86ff6cf1bd65d61651d10f2e5fa5c30bcb
kawsar uddin patoary <kawsaruddin450@gmail.com>
2024-10-31 19:19:15 +0600

    math functions in c++

---
+++ math_functions.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int x = 10;
+    int y = 5;
+
+    // cout << max(x, y);
+    // cout << pow(2, 3);
+    // cout << sqrt(9);
+    // cout << abs(-4);
+    cout << round(4.3);
+    return 0;
+}
